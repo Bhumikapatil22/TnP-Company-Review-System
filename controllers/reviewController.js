a342eda7146b345edfbd4830bc51ea7c7654af1d
Bhumika Patil <bhumikapatil2204@gmail.com>
2024-11-04 16:57:21 +0530

    Modified the code, added get request logic

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,26 +3,41 @@ const Company = require("../models/Company");
 
 exports.createReview = async (req, res) => {
   try {
-    const { company, alumni, review } = req.body;
+    const { company, alumni, reviews } = req.body;
 
-    const reviews = new Review({
+    const review = new Review({
       company,
       alumni,
-      review,
+      reviews,
     });
 
-    const savedReviews = await reviews.save();
+    const savedReviews = await review.save();
 
     const updatedCompany = Company.findByIdAndUpdate(
       company,
-      { $push: { createReview: savedReviews._id } },
+      { $push: { reviews: savedReviews._id } },
       { new: true }
     )
       .populate("reviews")
       .exec();
 
-    res.json({ company:updatedCompany });
+    res.json({ company: updatedCompany });
   } catch (error) {
     res.status(500).json({ message: "Error while adding Review " });
   }
 };
+
+exports.getAllReviews=async(req,res)=>{
+  try{
+    const reviews=await Review.find()
+    res.json({
+      reviews
+    })
+  }
+  catch(error)
+  {
+    return res.status(400).json({
+      error:"error while fetching post",
+  })
+  }
+}
