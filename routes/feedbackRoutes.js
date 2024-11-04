640716bf65c187e3179a60c269b59f73fe38a6ba
Bhumika Patil <bhumikapatil2204@gmail.com>
2024-11-04 16:58:40 +0530

    Added routes for all get requests for reviews,users and companies

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -1,12 +1,15 @@
 const express=require("express");
 const router=express.Router();
 
-const {createUser}=require("../controllers/userController");
-const {createCompany}=require("../controllers/companyController");
-const {createReview}=require("../controllers/reviewController");
+const {createUser,getAllUsers}=require("../controllers/userController");
+const {createCompany,getAllCompanies}=require("../controllers/companyController");
+const {createReview,getAllReviews}=require("../controllers/reviewController");
 
 router.post("/user/create",createUser);
+router.get("/user/get",getAllUsers);
 router.post("/company/create",createCompany);
+router.get("/company/get",getAllCompanies);
 router.post("/review/create",createReview);
+router.get("/review/get",getAllReviews);
 
 module.exports=router;
