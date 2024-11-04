118a2a8c916290c0e4015eb83c6cc277ae3656f6
Bhumika Patil <bhumikapatil2204@gmail.com>
2024-11-04 17:33:22 +0530

    Updated routes

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -4,6 +4,8 @@ const router=express.Router();
 const {createUser,getAllUsers}=require("../controllers/userController");
 const {createCompany,getAllCompanies}=require("../controllers/companyController");
 const {createReview,getAllReviews}=require("../controllers/reviewController");
+const {likeReviews}=require("../controllers/likeController");
+
 
 router.post("/user/create",createUser);
 router.get("/user/get",getAllUsers);
@@ -11,5 +13,6 @@ router.post("/company/create",createCompany);
 router.get("/company/get",getAllCompanies);
 router.post("/review/create",createReview);
 router.get("/review/get",getAllReviews);
+router.post("/likes/like",likeReviews);
 
 module.exports=router;
