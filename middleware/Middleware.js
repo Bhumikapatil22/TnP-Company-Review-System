711fbd4ab067b5e71b8e6c3b21cc8a3b34b8ddce
Bhumika Patil <bhumikapatil2204@gmail.com>
2024-11-05 17:23:13 +0530

    Created middleware to check whether user is Admin or Alumni

---
+++ middleware/Middleware.js
@@ -0,0 +1,43 @@
+const User=require("../models/User");
+
+
+exports.isAdmin=async(req,res,next)=>{
+    try{
+    const {email}=req.body;
+    const user=await User.findOne({email});
+
+    if(user && user.role==='Admin')
+    {
+        // Proceed if role is Admin
+        next();
+    }
+    else{
+        res.status(403).json({message:"Access Denied, only Admin can add comapanies"})
+    }
+}
+    catch(error)
+    {
+        res.status(500).json({ message: "Error verifying user role", error });
+    }
+};
+
+
+exports.isAlumni=async(req,res,next)=>{
+    try{
+       const {email}= req.body;
+       const user=await User.findOne({email});
+        if(user && user.role==='Alumni')
+        {
+            next();
+        }
+        else
+        {
+            res.status(403).json({message:"Access Denied, only Alumnis can add reviews"})
+        }
+
+    }
+    catch(error)
+    {
+        res.status(500).json({message:"Error verifying user role",error})
+    }
+}
