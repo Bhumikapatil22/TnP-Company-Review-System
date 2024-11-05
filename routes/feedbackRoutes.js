c28e528eb84ec4f4a4f8fd8ad5c1d8a738f0e211
Bhumika Patil <bhumikapatil2204@gmail.com>
2024-11-05 18:28:55 +0530

    Added all routes

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -1,17 +1,21 @@
 const express=require("express");
 const router=express.Router();
 
-const {createUser,getAllUsers}=require("../controllers/userController");
+const {createAdmin,createAlumni,createStudent,getAllUsers}=require("../controllers/userController");
 const {createCompany,getAllCompanies}=require("../controllers/companyController");
 const {createReview,getAllReviews}=require("../controllers/reviewController");
 const {likeReviews}=require("../controllers/likeController");
 
+//middleware
+const {isAdmin,isAlumni}=require("../middleware/Middleware");
 
-router.post("/user/create",createUser);
+router.post("/admin/create",createAdmin);
+router.post("/alumni/create",createAlumni);
+router.post("/student/create",createStudent);
 router.get("/user/get",getAllUsers);
-router.post("/company/create",createCompany);
+router.post("/company/create",isAdmin,createCompany);
 router.get("/company/get",getAllCompanies);
-router.post("/review/create",createReview);
+router.post("/review/create",isAlumni,createReview);
 router.get("/review/get",getAllReviews);
 router.post("/likes/like",likeReviews);
 
