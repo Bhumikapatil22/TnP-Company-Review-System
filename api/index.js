4b8c4ee8d4003f2a90f0da37d2c0982a6a080696
Bhumika Patil <148256877+Bhumikapatil22@users.noreply.github.com>
2026-06-23 00:24:53 +0530

    Add CORS middleware for frontend access
    Added CORS middleware to allow requests from the React frontend.

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dbConnect from '../config/database.js';
 import feedbackRoute from '../routes/feedbackRoutes.js';
 
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -11,7 +12,12 @@ dbConnect();
 
 // Middleware for parsing JSON requests
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // React/Vite frontend URL
+    credentials: true,
+  })
+);
 // Mount feedback routes
 app.use("/api/v1",feedbackRoute)//mounting
 
